a88018bb64f80def2964abc8f6c892030958bf36
Matheus Matos <matheus.matos@accurate.com.br>
2023-11-11 05:01:22 -0300

    Projeto 7

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7");
+
+
+        int idadeMatheus = 16;
+        int quantidadePessoas = 2;
+
+        if (idadeMatheus >= 16)
+        {
+            Console.WriteLine("Pode entrar");
+        }
+        else
+        {
+            if (quantidadePessoas >= 2)
+            {
+                Console.WriteLine("Pode entrar");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+        }
+
+
+
+        Console.WriteLine("Tecle qualquer coisa para finalizar!");
+        Console.ReadLine();
+    }
+}
+}
